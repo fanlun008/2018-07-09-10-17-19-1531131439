6074f7f9f14068487b07c60d471eb11e659da74e
AndrewLi <13058100289@163.com>
2019-07-11 23:43:53 +0800

    update

--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,87 @@
 package practice11;
 
-public class Teacher {
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+
+    private Klass klass;
+    private List<Klass> classes;
+
+    public List getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List classes) {
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age, List classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        if (this.classes == null || this.classes.isEmpty()) {
+            return String.format("%1$s I am a Teacher. I teach No Class.",super.introduce());
+        } else {
+            String clasString = "";
+            List<String> collect = this.classes.stream().map(value -> value.getNumber().toString()).collect(Collectors.toList());
+            Object[] objects = collect.toArray();
+            clasString = StringUtils.join(objects, ", ");
+            return String.format("%1$s I am a Teacher. I teach Class %2$s.",super.introduce(), clasString);
+        }
+
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student)){
+            return String.format("%1$s I am a Teacher. I teach %2$s.", super.introduce(), student.getName());
+        }else {
+            return String.format("%1$s I am a Teacher. I don't teach %2$s.", super.introduce(), student.getName());
+        }
+
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : this.classes) {
+            if (klass.getNumber() == student.getKlass().getNumber()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
